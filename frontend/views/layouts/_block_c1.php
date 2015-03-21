b616d264c6a5d2293b59b3d1c9830ec142dd9e9e
Oleg <oleg3012@yandex.ru>
2015-03-21 21:09:14 +0300

    dfhj

--- frontend/views/layouts/_block_c1.php
+++ frontend/views/layouts/_block_c1.php
@@ -2,18 +2,18 @@
     use yii\helpers\Html;
     use common\models\MainpageItem;
 ?>                
-        <h2 class="columntitle"><?= $title ?></h2>
+        <h2 class="columntitle" style="margin-left: 10px;"><?= $title ?></h2>
+        <ul class="news-list">
         <?php
             $i = 0;
             foreach($items as $k => $item) {
                 if($item['type'] == $type && $i < $num) {
 
                     $link = ['article/view', 'category_slug' => $item['category_slug'], 'slug' => $item['slug']]; ?>
-                    <div class="titles">
-                        <?= Html::a($item['title'], $link); ?>
-                    </div>
+                    <li><?= Html::a($item['title'], $link); ?></li>
                     <?php
                     ++$i;
                 }
             }
             ?>
+        </ul>
