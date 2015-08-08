23ca00b4d99313e033f21d289341b37e2784cc0a
Oleg <oleg3012@yandex.ru>
2015-08-08 09:54:26 +0300

    commit_stage_2015-08-08_09-54

--- frontend/views/layouts/_block_adv.php
+++ frontend/views/layouts/_block_adv.php
@@ -6,7 +6,7 @@ use common\models\MainpageItem;
 
 <div class="box-common" style="margin: 0 0 40px 0;">
     <!-- <h2 class="box-title"><span>Наши партнеры</span></h2> -->
-    <div class="box-header"><span>Наши партнеры</span></div>
+    <div class="box-header"><span style="text-align: center;">Наши партнеры</span></div>
 
     <a href="http://www.dinitrol.ru/">
         <img src="/images/dinitrol.jpg" style="max-width: 90%; height: auto; margin: -10px 0 0 10px;" />
